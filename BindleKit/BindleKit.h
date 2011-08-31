0d44b51c65dcd2bd7aa4de93a23f1270bf198612
David M. Syzdek <syzdek@bindlebinaries.com>
2011-08-30 17:20:21 -0800

    Creating initial Xcode project.

---
+++ BindleKit/BindleKit.h
@@ -0,0 +1,13 @@
+//
+//  BindleKit.h
+//  BindleKit
+//
+//  Created by David M. Syzdek on 8/30/11.
+//  Copyright 2011 Bindle Binaries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BindleKit : NSObject
+
+@end
